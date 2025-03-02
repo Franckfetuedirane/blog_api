23dc9cff2d193047b0fc6f1db63293bd5bae97fa
Franck Fetue <franckfetuef@gmail.com>
2025-03-02 17:25:56 +0100

    correction d'erreur

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': 'blog_api',
         'USER': 'postgres',
         'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
