ec8a31b8b157281a771c354a73793dff130a6252
Franck Fetue <franckfetuef@gmail.com>
2025-03-05 08:24:13 +0100

    docker

--- api/models.py
+++ api/models.py
@@ -4,14 +4,14 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     publication_date = models.DateField()
-    # author = models.()
+
     class Meta:
-     app_label = 'api'
+        app_label = 'api'
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField()
     country = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.name
